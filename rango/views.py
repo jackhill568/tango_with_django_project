1dca61c57a1ad2a16a7a89a3e831f35bd7b8f874
jack <jackhill9090@gmail.com>
2026-01-23 12:46:08 +0000

    chapter three complete

---
+++ rango/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Rango says hey there partner!\n <a href="/rango/about/">about</a> ')
+
+def about(request):
+    return HttpResponse("Rango says here is the about page\n <a href='/rango/'>index</a>")
+
+
+
+
+
